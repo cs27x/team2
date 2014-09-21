b9643a4fa485e6076adef4a3be14fa25b7a85cca
Jejo Koola <jdkoola@gmail.com>
2014-09-21 16:02:14 -0500

    Added build.gradle

---
+++ asgn1/assigned_project/code/src/test/java/edu/vanderbilt/cs278/team2/assign1/AppTest.java
@@ -0,0 +1,26 @@
+package edu.vanderbilt.cs278.team2.assign1;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import java.util.List;
+
+import org.joda.time.DateTime;
+import org.junit.Test;
+
+public class AppTest {
+
+	
+	
+	@Test
+	public void test1() throws Exception {
+	
+	}
+	
+	@Test
+	public void test2() throws Exception {
+
+	}
+
+}
