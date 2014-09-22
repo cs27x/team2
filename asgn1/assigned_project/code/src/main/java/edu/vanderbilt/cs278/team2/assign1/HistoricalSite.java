fe69a30417a1a8bfdcaaecf4b1b5762a362838e7
Jejo Koola <jdkoola@gmail.com>
2014-09-22 10:12:55 -0500

    Got JSONParser working

--- asgn1/assigned_project/code/src/main/java/edu/vanderbilt/cs278/team2/assign1/HistoricalSite.java
+++ asgn1/assigned_project/code/src/main/java/edu/vanderbilt/cs278/team2/assign1/HistoricalSite.java
@@ -1,14 +1,23 @@
 package edu.vanderbilt.cs278.team2.assign1;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class HistoricalSite {
 	private boolean visited;
 	
-	private String displayName;
-	private String locationDescription;
-	private double latitude;
-	private double longitude;
+	@JsonProperty("title") private String displayName;
+	@JsonProperty("location") private String locationDescription;
+	@JsonProperty("latitude") private double latitude;
+	@JsonProperty("longitude") private double longitude;
 	private String historyDescription;
-	private Location location = new Location();
+	@JsonProperty("marker_text") private Location location = new Location();
+	
+	public HistoricalSite() {
+		super();
+	}
 	
 	 @Override
 	public boolean equals(Object o) {
@@ -28,8 +37,8 @@ public class HistoricalSite {
 	        HistoricalSite c = (HistoricalSite) o;
 	         
 	        // Compare the data members and return accordingly 
-	        return Double.compare(longitude, c.getLocation().getLongitude()) == 0
-	                && Double.compare(latitude, c.getLocation().getLatitude()) == 0
+	        return Double.compare(getLongitude(), c.getLocation().getLongitude()) == 0
+	                && Double.compare(getLatitude(), c.getLocation().getLatitude()) == 0
 	                && historyDescription == c.getHistoryDescription()
 	                && displayName == c.getDisplayName()
 	        		&& locationDescription == c.getLocationDescription();
@@ -50,18 +59,22 @@ public class HistoricalSite {
 		visited = false;
 	}
 	
+	@JsonProperty("name")
 	public void setDisplayName(String name) {
 		displayName = name;
 	}
 	
+	@JsonProperty("name")
 	public String getDisplayName() {
 		return displayName;
 	}
 	
+	@JsonProperty("location")
 	public void setLocationDescription(String description) {
 		locationDescription = description;
 	}
 	
+	@JsonProperty("location")
 	public String getLocationDescription() {
 		return locationDescription;
 	}
@@ -76,11 +89,33 @@ public class HistoricalSite {
 		return location;
 	}	
 	
+	@JsonProperty("marker_text")
 	public void setHistoryDescription(String description) {
 		historyDescription = description;
 	}
 	
+	@JsonProperty("marker_text")
 	public String getHistoryDescription() {
 		return historyDescription;
+	}
+	
+	@JsonProperty("latitude")
+	public double getLatitude() {
+		return latitude;
+	}
+	
+	@JsonProperty("latitude")
+	public void setLatitude(double latitude) {
+		this.latitude = latitude;
+	}
+
+	@JsonProperty("longitude")
+	public double getLongitude() {
+		return longitude;
+	}
+
+	@JsonProperty("longitude")
+	public void setLongitude(double longitude) {
+		this.longitude = longitude;
 	}
 }
--- asgn1/assigned_project/code/src/main/java/edu/vanderbilt/cs278/team2/assign1/HistoricalSiteDataProvider.java
+++ asgn1/assigned_project/code/src/main/java/edu/vanderbilt/cs278/team2/assign1/HistoricalSiteDataProvider.java
@@ -1,5 +1,9 @@
 package edu.vanderbilt.cs278.team2.assign1;
 
+
+import com.fasterxml.jackson.databind.JavaType;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 public class HistoricalSiteDataProvider {
-	
+	public static final String NASHVILLE_HISTORICAL_SITES_URL = "https://data.nashville.gov/resource/2j6c-58gf.json";
 }
