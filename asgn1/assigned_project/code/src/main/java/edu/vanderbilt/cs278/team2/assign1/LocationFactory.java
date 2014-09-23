7eef3de1963e0ec090705a395d09140e20a96fb7
schoenmh <Michael@michael-pc>
2014-09-23 14:08:22 -0500

    Removed Location.java (replaced with myLocation class) added LocationTracker classes. These get used by the LocationFactory. The LocationFactory will be passed COntext from the Activity which is attempting to get the current location.  Allowing the Factory to return myLocation objects, which can then call distanceTo between two myLocation objects.

--- asgn1/assigned_project/code/src/main/java/edu/vanderbilt/cs278/team2/assign1/LocationFactory.java
+++ asgn1/assigned_project/code/src/main/java/edu/vanderbilt/cs278/team2/assign1/LocationFactory.java
@@ -1,5 +1,50 @@
-package edu.vanderbilt.cs278.team2.assign1;
 
-public class LocationFactory {
+import android.location.*;
+
+import android.location.LocationListener;
+
+import com.google.android.gms.location.*;
+import com.example.nashpast.myLocation;
+
+import android.app.Service;
+import android.content.*;
+
+import java.util.List;
+import java.util.Locale;
+
+public class LocationFactory{
+	
+	
+	public myLocation createLocationFromAddress(String address, Context context)
+	{
+		Geocoder geoCoder = new Geocoder(context, Locale.getDefault());
+		    if (address != null && !address.isEmpty()) {
+		        try {
+		            List<Address> addressList = geoCoder.getFromLocationName(address, 1);
+		            if (addressList != null && addressList.size() > 0) {
+		                double lat = addressList.get(0).getLatitude();
+		                double lng = addressList.get(0).getLongitude();
+		                myLocation myLoc = new myLocation(lat,lng);
+		                return myLoc;
+		            }
+		        } catch (Exception e) {
+		            e.printStackTrace();
+		        } // end catch
+		    } // end if
+			return null;
+	}
+
+	public myLocation getCurrentLocation(Context context)
+	{
+		LocationTracker myLocTracker = new FallbackLocationTracker(context);
+		return new myLocation(myLocTracker.getLocation());
+	}
+	
+	public myLocation getLocationLatLong(double lat, double lng)
+	{
+		return new myLocation(lat,lng);
+	}
+	
+	
 	
 }
