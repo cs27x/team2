d4c079c9a9ad72d589ed607368789c6194f89686
Oliver Dormody <odormody@gmail.com>
2014-09-23 09:07:43 -0500

    Added visited flag and equality operator to HistoricalSites

--- asgn1/assigned_project/code/src/main/java/edu/vanderbilt/cs278/team2/assign1/HistoricalSite.java
+++ asgn1/assigned_project/code/src/main/java/edu/vanderbilt/cs278/team2/assign1/HistoricalSite.java
@@ -2,4 +2,83 @@ package edu.vanderbilt.cs278.team2.assign1;
 
 public class HistoricalSite {
 	
+	private String displayName;
+	private String locationDescription;
+	private double latitude;
+	private double longitude;
+	private String historyDescription;
+	private boolean isVisited;
+	
+	 @Override
+	public boolean equals(Object o) {
+	 
+	        // If the object is compared with itself then return true  
+	        if (o == this) {
+	            return true;
+	        }
+	 
+	        /* Check if o is an instance of Complex or not
+	          "null instanceof [type]" also returns false */
+	        if (!(o instanceof HistoricalSite)) {
+	            return false;
+	        }
+	         
+	        // typecast o to Complex so that we can compare data members 
+	        HistoricalSite c = (HistoricalSite) o;
+	         
+	        // Compare the data members and return accordingly 
+	        return Double.compare(longitude, c.getLongitude()) == 0
+	                && Double.compare(latitude, c.getLatitude()) == 0
+	                && historyDescription == c.getHistoryDescription()
+	                && displayName == c.getDisplayName()
+	        		&& locationDescription == c.getLocationDescription();
+	}
+	 
+	public void setDisplayName(String name) {
+		displayName = name;
+	}
+	
+	public String getDisplayName() {
+		return displayName;
+	}
+	
+	public void setLocationDescription(String description) {
+		locationDescription = description;
+	}
+	
+	public String getLocationDescription() {
+		return locationDescription;
+	}
+	
+	public void setLatitude(double lat) {
+		latitude = lat;
+	}
+	
+	public double getLatitude() {
+		return latitude;
+	}
+	
+	public void setLongitude(double inLong) {
+		longitude = inLong;
+	}
+	
+	public double getLongitude() {
+		return longitude;
+	}
+	
+	public void setHistoryDescription(String description) {
+		historyDescription = description;
+	}
+	
+	public String getHistoryDescription() {
+		return historyDescription;
+	}
+	
+	public void setIsVisited(boolean visited) {
+		isVisited = visited;
+	}
+	
+	public boolean isVisited() {
+		return isVisited;
+	}
 }
