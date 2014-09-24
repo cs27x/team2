708ed2d73e5392e00fc85d25b423fbca6d906f1f
schoenmh <Michael@michael-pc>
2014-09-24 17:31:18 -0500

    Added some comments.

--- asgn1/assigned_project/code/src/main/java/edu/vanderbilt/cs278/team2/assign1/LocationFactory.java
+++ asgn1/assigned_project/code/src/main/java/edu/vanderbilt/cs278/team2/assign1/LocationFactory.java
@@ -14,13 +14,18 @@ import java.util.Locale;
 
 public class LocationFactory{
 	
-	
+	/**
+	 * @author Neutrino with changes by Michael Schoenfield
+	 * takes in an address and context, return a myLocation object containing
+	 * the desired latitude and longitude
+	 * Uses Geocoder
+	 */
 	public myLocation createLocationFromAddress(String address, Context context)
 	{
-		Geocoder geoCoder = new Geocoder(context, Locale.getDefault());
+		Geocoder mGeoCoder = new Geocoder(context, Locale.getDefault());
 		    if (address != null && !address.isEmpty()) {
 		        try {
-		            List<Address> addressList = geoCoder.getFromLocationName(address, 1);
+		            List<Address> addressList = mGeoCoder.getFromLocationName(address, 1);
 		            if (addressList != null && addressList.size() > 0) {
 		                double lat = addressList.get(0).getLatitude();
 		                double lng = addressList.get(0).getLongitude();
@@ -33,13 +38,18 @@ public class LocationFactory{
 		    } // end if
 			return null;
 	}
-
+	/**
+	 * Relies on the interface Location Tracker in order to create a location object
+	 * with the current address.
+	 */
 	public myLocation getCurrentLocation(Context context)
 	{
 		LocationTracker myLocTracker = new FallbackLocationTracker(context);
 		return new myLocation(myLocTracker.getLocation());
 	}
-	
+	/**
+	 * Takes in Lat and Longitude in the form of doubles, and returns a mylocation
+	 */
 	public myLocation getLocationLatLong(double lat, double lng)
 	{
 		return new myLocation(lat,lng);
